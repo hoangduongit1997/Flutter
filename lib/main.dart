063d610545c916bec0c5d5391f7e93121b2c9289
hoangduongit1997 <hoangduongit1997@gmail.com>
2019-08-03 12:55:26 +0700

    Unit Test Simple
    Test Http request

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
-void main() => runApp(MyApp());
+import 'src/app.dart';
 
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    final word=WordPair.random();
-    return MaterialApp(
-      title: 'Random App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Random Word'),
-        ),
-        body: Center(
-          child: Randomword(),
-        ),
-      ),
-    );
-  }
+void main() {
+  runApp(App());
 }
-class RandomWordState extends State<Randomword>
-{
-  @override
-  Widget build(BuildContext context) {
-    final word=WordPair.random();
-    return Text(word.asUpperCase);
-  }
-}
-
-class Randomword extends StatefulWidget{
-  @override
-  State<StatefulWidget> createState() {
-    // TODO: implement createState
-    return new RandomWordState();
-  }
-
-}
-
